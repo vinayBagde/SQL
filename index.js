003609f2cb8ab505bcf6091b317414daaf5fb530
vinayBagde <vinaybagde0@gmail.com>
2025-01-10 05:26:28 +0530

    create edit route

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const mysql = require("mysql2");
 const express = require("express");
 const app = express();
 const path = require("path");
+const methodOverride = require("method-override");
+
+
+app.use(methodOverride("_method"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
@@ -10,9 +14,8 @@ app.set("views", path.join(__dirname, "/views"));
 app.use(express.static("public"));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -56,7 +59,7 @@ app.get("/", (req, res) => {
         console.log(result);
         // console.log(result[0]["count(*)"]);
         let count = result[0]["count(*)"];
-        res.render("home.ejs", {count});
+        res.render("home.ejs", { count });
       }
     );
   } catch (err) {
@@ -66,24 +69,53 @@ app.get("/", (req, res) => {
   // res.send("welcome to home page");
 });
 
-
 app.get("/users", (req, res) => {
   let q = `SELECT * FROM user`;
   try {
     connection.query(q, (err, users) => {
-      if(err) throw err;
-      res.render("showUsers.ejs", {users});
-    })
-  } catch(err) {
+      if (err) throw err;
+      res.render("showUsers.ejs", { users });
+    });
+  } catch (err) {
     res.send("some error occurred");
   }
 });
+
+app.get("/user/:id/edit", (req, res) => {
+  let { id } = req.params;
+  let q = `SELECT * FROM user WHERE id='${id}'`;
+  try {
+    connection.query(q, (err, result) => {
+      if (err) throw err;
+      let user = result[0]
+      res.render("edit.ejs", { user });
+      console.log(result);
+    });
+  } catch (err) {
+    res.send("some error occurred");
+  }
+});
+
+
+app.patch("/user/:id", (req, res) => {
+  let { id } = req.params;
+  let q = `SELECT * FROM user WHERE id='${id}'`;
+  try {
+    connection.query(q, (err, result) => {
+      if (err) throw err;
+      let user = result[0];
+      res.render("edit.ejs", { user });
+      console.log(result);
+    });
+  } catch (err) {
+    res.send("some error occurred");
+  }
+})
 
 const port = 8080;
 
 app.listen(port, () => {
   console.log("sever is listening to port 8080");
 });
-
 
 // connection.end();
